99da2505f5f2b17f10a29f24f5703580156f9c26
vinsnt76 <83260589+vinsnt76@users.noreply.github.com>
2026-07-08 23:51:46 +0800

    fix: update dynamic sitemap generator with new case studies route nodes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -149,5 +149,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.7,
     },
+    {
+      url: `${baseUrl}/knowledge-hub/case-studies`,
+      lastModified: currentDate,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/knowledge-hub/case-studies/baby-bento`,
+      lastModified: currentDate,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
   ];
 }
