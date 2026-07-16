2b1456b82351d2faaa3d2fb48631e46dedf2a3be
vinsnt76 <83260589+vinsnt76@users.noreply.github.com>
2026-07-16 18:26:59 +0800

    feat: route google tags through custom /metrics endpoint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -124,7 +124,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M9F4DT3Q"
+            src="/metrics/ns.html?id=GTM-M9F4DT3Q"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
@@ -157,14 +157,14 @@ export default function RootLayout({
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            '/metrics/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-M9F4DT3Q');
           `}
         </Script>
 
         {/* Google Analytics Global Site Tag (gtag.js) */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-3WVLWVG6VH"
+          src="/metrics/gtag/js?id=G-3WVLWVG6VH"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
